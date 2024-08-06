d5bc1c624474dce4a482dcaf31f862f1a40c5095
Codie Stephens-Evans <codie18@gmail.com>
2024-08-06 10:53:00 +0100

    foxes

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div className='w-full h-[80px] flex flex-row gap-4 items-center justify-start px-4 py-2 bg-gray-300 rounded-b-xl text-xl'>
-      <Link className='text-black hover:text-white transition-all duration-200 ease' to='/'>Home</Link>
-      <Link className='text-black hover:text-white transition-all duration-200 ease' to='/contact'>Contact</Link>
+      <Link className='text-black transition-all duration-200 hover:text-white ease' to='/'>Home</Link>
+      <Link className='text-black transition-all duration-200 hover:text-white ease' to='/contact'>Contact</Link>
 
-      <FaSearch className='text-black hover:text-white transition-all duration-200 ease cursor-pointer' />
+      <FaSearch className='text-black transition-all duration-200 cursor-pointer hover:text-white ease' />
     </div>
   )
 }
