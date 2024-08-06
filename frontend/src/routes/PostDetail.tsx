b8301ebf7c5a88e68d7b53b3c9cb3c2d5cfd3e74
Codie Stephens-Evans <codie18@gmail.com>
2024-08-07 00:16:52 +0100

    added api route for get a post

--- frontend/src/routes/PostDetail.tsx
+++ frontend/src/routes/PostDetail.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { PostProp } from '../../types/types';
 import { useParams } from "react-router-dom";
+
 const PostDetail = () => {
+  // Declaration
   const { id } = useParams();
+
+  // UseStates
+  const [post, setPost] = useState<PostProp>()
+
+  const [fetchError, setFetchError] = useState(false)
+  const [loading, setLoading] = useState(true);
+
+
+
+  // UseEffects
+  useEffect(() => {
+    fetchPost(Number(id))
+  }, [])
+
+  // Functions
+  const fetchPost = async (id: number) => {
+    try {
+      // setLoading(true);
+      // setFetchError(false);
+      const res = await fetch(`${process.env.REACT_APP_PUBLIC_URL}/api/post?id=${id}`);
+      const post = await res.json();
+      console.log(`${process.env.REACT_APP_PUBLIC_URL}/api/post?id=${id}`);
+      // console.log(post);
+      // setPost(post.post);
+      // setLoading(false);
+    } catch (error) {
+      // setLoading(false);
+      console.log(error);
+      // setFetchError(true);
+    }
+  }
 
   return (
     <div>PostDetail for {id}</div>
